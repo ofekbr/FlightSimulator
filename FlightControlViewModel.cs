04fb31f50d2dd293d5a64f6a2d67e301f990963c
ofek <barkai.ofek@gmail.com>
2020-04-10 19:31:11 +0300

    joystick and slider's values are now updated in VM

--- FlightControlViewModel.cs
+++ FlightControlViewModel.cs
@@ -76,18 +76,38 @@ namespace FlightSimulatorApp
         public double VM_Rudder
         {
             get { return model.Rudder; }
+            set
+            {
+                model.Rudder = value;
+                this.NotifyPropertyChanged("Rudder");
+            }
         }
         public double VM_Elevator
         {
             get { return model.Elevator; }
+            set
+            {
+                model.Elevator = value;
+                this.NotifyPropertyChanged("Elevator");
+            }
         }
         public double VM_Aileron
         {
             get { return model.Aileron; }
+            set
+            {
+                model.Aileron = value;
+                this.NotifyPropertyChanged("Aileron");
+            }
         }
         public double VM_Throttle
         {
             get { return model.Throttle; }
+            set
+            {
+                model.Throttle = value;
+                this.NotifyPropertyChanged("Throttle");
+            }
         }
         
         public double VM_Latitude
